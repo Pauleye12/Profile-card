6b2becf2b91b117959a2c31aba69d3d22b60294c
pauleye <orepaul12@gmail.com>
2022-09-09 00:39:41 +0100

    edited adress and info component

--- src/components/address.jsx
+++ src/components/address.jsx
@@ -4,7 +4,7 @@ function Address() {
   return (
     <div className="flex flex-col gap-2 w-full mt-4">
       <h1 className="text-center text-[25px] font-[700]">Our office</h1>
-      <p className=''>Gwarinpa Estate,Gwarinpa, Abuja.</p>
+      <p className=''>ALD Estate, 9th Avenue Gwarinpa, Galadinma. Abuja.</p>
     </div>
   );
 }
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -5,7 +5,9 @@ function Info() {
     <div className="flex flex-col justify-center items-center gap-1">
       <h2 className="text-[25px] font-[700] ">Igoji Caroline</h2>
       <h3 className="text-[16px] font-[400]">Fashion Designer</h3>
-      <h4 className="text-[13px] font-[400]">CEO Ceec Clothing</h4>
+      <h4 className="text-[13px] font-[400]">
+        CEO <b>CEES Clothing</b>{" "}
+      </h4>
       <section className="flex justify-center gap-[20px] mt-2">
         <a
           href="https://www.snapchat.com/add/obiye2020?share_id=j8OpyvmzgN0&locale=en-GB"
